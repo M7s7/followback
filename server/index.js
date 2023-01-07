69ee1279acc03f1a6f7285b6b0cf062c1b5e1f75
Michael Siu <michaelsiu01@gmail.com>
2023-01-08 01:21:35 +1100

    Temporarily removed Strictmode to relieve rate limit

--- server/index.js
+++ server/index.js
@@ -1,15 +1,19 @@
+const cors = require('cors')
 require('dotenv').config()
 const express = require('express')
 const app = express()
-const cors = require('cors')
-const { getID, getFollowing, searchTweets } = require('./routes/api-twitter')
+const { getID, getFollowing, searchTweets, IDstoUsers } = require('./routes/api-twitter')
 
 // Middleware routes
 app.use(cors())
 app.use('/ID', getID)
 app.use('/following', getFollowing)
 app.use('/mentions', searchTweets)
-//app.use('/toUsers', IDstoUsers)
+app.use('/ids-to-users', IDstoUsers)
+
+app.get('/test', (req, res) => {
+  res.send("API TEST COMPLETED")
+})
 
 
 const port = process.env.PORT || 3001
