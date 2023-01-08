84746bb776b80fb8740856941b97e05db29f5bd6
Michael Siu <michaelsiu01@gmail.com>
2023-01-08 20:36:50 +1100

    Added mutual follower and status elements

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,18 +5,15 @@ const App = () => {
   const [names, setNames] = useState([])
   const [buttonState, toggleButton] = useState(0)
   const [data, setData] = useState([])
-  const [mutuals, setMutuals] = useState([])
-
-  //useEffect(() => {
-    //setNames(["LauraPrepon", "tayschilling"])
-  //}, [])
 
   const fetchTwitterData = async (name1, name2) => {
     const ID1 = await userID(name1)
     const friends1 = await userFollowing(ID1.data.id)
 
     const userData1 = {
-      ID: ID1.data,
+      ID: ID1.data.id,
+      name: ID1.data.name,
+      username: ID1.data.username,
       friends: friends1._realData.ids
     }
 
@@ -24,14 +21,20 @@ const App = () => {
     const friends2 = await userFollowing(ID2.data.id)
 
     const userData2 = {
-      ID: ID2.data,
+      ID: ID2.data.id,
+      name: ID2.data.name,
+      username: ID2.data.username,
       friends: friends2._realData.ids
     }
 
     const mutualList = await getMutuals(userData1.friends, userData2.friends)
     console.log(mutualList)
-    setMutuals(mutualList)
-    setData([userData1, userData2])
+
+    setData({
+      user1: userData1,
+      user2: userData2,
+      mutuals: mutualList
+    })
   }
 
 
@@ -43,38 +46,90 @@ const App = () => {
 
   return (
     <div>
-      <Display names={["LauraPrepon", "tayschilling"]} data={data} fetchData={fetchTwitterData} mutuals={mutuals} />
+      <Display names={["LauraPrepon", "samirawiley"]} data={data} fetchData={fetchTwitterData} />
     </div>
   )
 }
 
 
-const Display = ({ names, data, fetchData, mutuals }) => {
+const Display = ({ names, data, fetchData }) => {
   useEffect(() => {
     fetchData(names[0], names[1])
     console.log(data)
   }, [])
 
-  if (data.length != 2) {
+  if (data.length === 0) {
     return (
       <>
-      Currently loading...
+        Enter two usernames to get started!
       </>
     )
   }
-  console.log(data)
 
-  console.log(mutuals, mutuals.length)
+
+  console.log(data)
+  console.log(data.mutuals, data.mutuals.length)
+
+  return (
+    <div>
+      State variables: {names[0]} {names[1]}
+      <MutualFollowingStatus data={data} />
+    </div>
+  )
+}
+
+
+
+const MutualFollowingStatus = ({ data }) => {
+  const ID1 = data.user1.ID
+  const ID2 = data.user2.ID
+
+  const name1 = data.user1.username
+  const name2 = data.user2.username
+  
+  const friendsList1 = data.user1.friends
+  const friendsList2 = data.user2.friends
+
+  const oneFollowsTwo = friendsList1.includes(ID2)
+  const twoFollowsOne = friendsList2.includes(ID1)
+
+  let status;
+  if (oneFollowsTwo && twoFollowsOne) {
+      status = <div> 
+          @{name1} and @{name2} are following each other.
+        </div>
+  } else if (oneFollowsTwo) {
+    status = <div> 
+        @{name1} follows @{name2}, but the feeling is not mutual. 
+      </div>
+  } else if (twoFollowsOne) {
+    status = <div> 
+        @{name2} follows @{name1}, but the feeling is not mutual. 
+      </div>
+  } else {
+    status = <div> 
+        @{name1} and @{name2} do not follow each other. 
+      </div>
+  } 
+
+  const eachUser = (user) => {
+    return (
+      <div>
+        {user.name} 
+        <a href={"https://twitter.com/" + user.username}> (@{user.username})</a> 
+      </div>
+    )
+  }
 
 
   return (
     <div>
-      These two users are {names[0]} and {names[1]}
-      {names[0]} has {} friends
-
-
+      {status}
+      Furthermore, there are {data.mutuals.data.length} users that they both follow: 
+      {data.mutuals.data.map((user) => eachUser(user))}
     </div>
   )
 }
+
 
 export default App
