7bc3e9e80e8dabddcae7efbabd8fc2d92adb35dd
Michael Siu <michaelsiu01@gmail.com>
2023-01-09 14:03:24 +1100

    Minor changes

--- server/index.js
+++ server/index.js
@@ -11,11 +11,6 @@ app.use('/following', getFollowing)
 app.use('/mentions', searchTweets)
 app.use('/ids-to-users', IDstoUsers)
 
-app.get('/test', (req, res) => {
-  res.send("API TEST COMPLETED")
-})
-
-
 const port = process.env.PORT || 3001
 app.listen(port, () => {
   console.log(`App is listening on ${port}`)
