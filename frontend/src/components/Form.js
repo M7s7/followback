3d28008bc08757984c4fd80ba90fafed62002f6c
Michael Siu <michaelsiu01@gmail.com>
2023-01-09 14:03:09 +1100

    Minor changes

--- frontend/src/components/Form.js
+++ frontend/src/components/Form.js
@@ -1,9 +1,10 @@
 const Form = ({ inputs, handleInput, handleSubmit }) => {
   return (
     <div>
-        <input value={inputs[0]} id="user1" onChange={handleInput} />
-        <input value={inputs[1]} id="user2" onChange={handleInput} />
-        <button onClick={handleSubmit}> GO! </button>
+        Do @<input value={inputs[0]} id="user1" onChange={handleInput} />
+        and @<input value={inputs[1]} id="user2" onChange={handleInput} />
+        follow each other?
+        <button onClick={handleSubmit}> Go! </button>
     </div>
   )
 }
